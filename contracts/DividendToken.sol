589c28c866eb4da86410a193d1b47ea8bd138487
tserg <tse.rong.gary@gmail.com>
2019-10-02 00:12:46 +0800

    first commit

--- contracts/DividendToken.sol
+++ contracts/DividendToken.sol
@@ -1,7 +1,33 @@
 pragma solidity ^0.5.0; 
 
 import "./ERC20Mintable.sol";
+import "./math/SafeMath.sol";
 
 contract dividendToken is ERC20Mintable {
-	
+
+	using SafeMath for uint256;
+
+	uint256 public totalDividends;
+
+	mapping (address => uint256) lastDividends;
+
+	function () external payable {
+		totalDividends = totalDividends.add(msg.value);
+	}
+
+	function dividendBalanceOf(address account) public view returns (uint256) {
+		uint256 newDividends = totalDividends.sub(lastDividends[account]);
+		uint256 product = balanceOf(msg.sender).mul(newDividends);
+
+		return product.div(totalSupply());
+	}
+
+	function claimDividend() public {
+		uint256 owing = dividendBalanceOf(msg.sender);
+		if (owing>0) {
+			msg.sender.transfer(owing);
+			lastDividends[msg.sender] = totalDividends;
+		}
+	}
+
 }
